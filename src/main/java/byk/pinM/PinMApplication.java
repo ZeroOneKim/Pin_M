236e1905c1269d7fe9285d438c042611bc41901a
Yungil Kim <wowwow0124@naver.com>
2023-08-19 13:38:53 +0900

    First init

---
+++ src/main/java/byk/pinM/PinMApplication.java
@@ -0,0 +1,13 @@
+package byk.pinM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/byk/pinM/PinMApplicationTests.java
@@ -0,0 +1,13 @@
+package byk.pinM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
