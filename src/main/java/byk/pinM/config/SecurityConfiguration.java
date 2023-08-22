3dc85d1d2e2e6fd112d1b23d25dc6b2fdd34a5fd
Yungil Kim <wowwow0124@naver.com>
2023-08-22 19:23:27 +0900

    Spring Security Setting

---
+++ src/main/java/byk/pinM/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package byk.pinM.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/signUp", "/check-mail",
+                        "/login-link", "/asd").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    //static 폴더 하위는 모두 접근가능 ex) css/ js ....
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
