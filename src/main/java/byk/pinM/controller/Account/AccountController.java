453d35ab7a4c5145ffafcfa93f19bb593e3460bb
Yungil Kim <wowwow0124@naver.com>
2023-08-24 20:09:54 +0900

    Valid Check

--- src/main/java/byk/pinM/controller/Account/AccountController.java
+++ src/main/java/byk/pinM/controller/Account/AccountController.java
@@ -1,6 +1,7 @@
 package byk.pinM.controller.Account;
 
-import byk.pinM.response.SignUpResponse;
+import byk.pinM.entity.Account.SignUpResponse;
+import byk.pinM.service.SignUpResponseValid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -10,17 +11,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
+
+    private SignUpResponseValid signUpResponseValid;
+    public void SignUpResponseValidMethod (SignUpResponseValid signUpResponseValid) {
+        this.signUpResponseValid = signUpResponseValid;
+    }
+
     @GetMapping("/signUp")
     public String signUpForm(Model model) {
+        model.addAttribute("signUpResponse", new SignUpResponse());
         return "account/signUp";
     }
 
     @PostMapping("signUp")
     public String signUpResponse(@ModelAttribute SignUpResponse signUpResponse, Errors errors) { //Error Code 작성 필요.
+        /*SignUpResponseValidMethod(signUpResponseValid);
+        signUpResponseValid.validate(signUpResponse, errors);*/
         if(errors.hasErrors()) {
             System.out.println("failed");
+            System.out.println("ERROR STATUS(값 체크) : " + errors.hasErrors());
             return "account/signUp";  //에러처리 예정
         }
+
+        System.out.println("ERROR STATUS(값 체크) : " + errors.hasErrors());
+
         System.out.println(signUpResponse);
         System.out.println("Success");
         return "redirect:/";
---
+++ src/main/java/byk/pinM/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package byk.pinM.repository;
+
+import byk.pinM.entity.Account.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<User, String> {
+    boolean existsById(String user_id);
+
+
+}
