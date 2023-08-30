c9880ede1a305fcaf548de8d1cfe7fb100514ed5
Yungil Kim <wowwow0124@naver.com>
2023-08-30 20:15:59 +0900

    Improve the Code And Log4j2

--- src/main/java/byk/pinM/service/SMTP/SignUpEmailChk.java
+++ src/main/java/byk/pinM/service/SMTP/SignUpEmailChk.java
@@ -4,31 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class SignUpEmailChk {
-    private MailSenderSMTP mailSenderSMTP;
-    private  String chkEmail_Id;
+    Logger logger = LogManager.getLogger("Email Id Chk Logic ======================================");
+    private EmailAndVefrifiactionCode emailAndVefrifiactionCode;
 
+    private MailSenderSMTP mailSenderSMTP;
     @Autowired
-    public void setMailSenderSMTP(MailSenderSMTP mailSenderSMTP) {
+    public void SignUpEmailChk(MailSenderSMTP mailSenderSMTP) {
         this.mailSenderSMTP = mailSenderSMTP;
     }
 
-    public void setChkEmail_Id(String chkEmail_Id) {
-        this.chkEmail_Id = chkEmail_Id;
+    @Autowired
+    public void setEmailAndVefrifiactionCode(EmailAndVefrifiactionCode emailAndVefrifiactionCode) {
+        this.emailAndVefrifiactionCode = emailAndVefrifiactionCode;
     }
 
-    public void sendEmailAndGenerateRandomNumber() {
-        if (chkEmail_Id != null) {
+    public void sendEmailAndGenerateRandomNumber(String chkEmail_Id) {
+        if (!chkEmail_Id.equals("")) {
             String val = RandomNumber();
+            logger.info("객체 테스트 ============================================================\n" +emailAndVefrifiactionCode);
             mailSenderSMTP.sendEmailforSignUp(chkEmail_Id, val);
+            emailAndVefrifiactionCode.setEmailMapping(chkEmail_Id, val);
         }
     }
 
     String RandomNumber() {
         Random random = new Random();
-        return "1561";
+        int intAnswer = random.nextInt(8999)+1000;
+        String answer = String.valueOf(intAnswer);
+        return answer;
     }
 
 
