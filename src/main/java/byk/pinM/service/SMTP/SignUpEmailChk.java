9097ce4ae0ad91c8399dcfe0c39a163183e6fb1d
Yungil Kim <wowwow0124@naver.com>
2023-09-01 20:03:20 +0900

    organize

--- src/main/java/byk/pinM/service/SMTP/SignUpEmailChk.java
+++ src/main/java/byk/pinM/service/SMTP/SignUpEmailChk.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Service
 public class SignUpEmailChk {
-    Logger logger = LogManager.getLogger("Email Id Chk Logic ======================================");
     private EmailAndVefrifiactionCode emailAndVefrifiactionCode;
 
     private MailSenderSMTP mailSenderSMTP;
@@ -26,7 +23,6 @@ public class SignUpEmailChk {
     public void sendEmailAndGenerateRandomNumber(String chkEmail_Id) {
         if (!chkEmail_Id.equals("")) {
             String val = RandomNumber();
-            logger.info("객체 테스트 ============================================================\n" +emailAndVefrifiactionCode);
             mailSenderSMTP.sendEmailforSignUp(chkEmail_Id, val);
             emailAndVefrifiactionCode.setEmailMapping(chkEmail_Id, val);
         }
