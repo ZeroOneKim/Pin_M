b51e34dba9152707bc349e4c534dff24cd1251d5
Yungil Kim <wowwow0124@naver.com>
2023-09-01 21:33:08 +0900

    Organize source

--- src/main/java/byk/pinM/service/SMTP/EmailAndVerificationCode.java
+++ src/main/java/byk/pinM/service/SMTP/EmailAndVerificationCode.java
@@ -1,20 +1,23 @@
 package byk.pinM.service.SMTP;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Stack;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class EmailAndVerificationCode {
     private HashMap<String, String> emailMapping = new HashMap<>();
-    private Timer timer = new Timer();
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     public void setEmailMapping(String email, String code) {
         this.emailMapping.put(email, code);
-
-        timer.schedule(new RemoveEmailMappingCode(email), 30000);
+        RemoveEmailMappingCode(email);
     }
 
     public boolean isValidCheckCode(String email, String password) {
@@ -23,16 +26,16 @@ public class EmailAndVerificationCode {
         return false;
     }
 
-    private class RemoveEmailMappingCode extends TimerTask {
-        private String willRemoveEmail;
-
-        private RemoveEmailMappingCode(String willRemoveEmail) {
-            this.willRemoveEmail = willRemoveEmail;
-        }
-
-        public void run() {
-            emailMapping.remove(willRemoveEmail);
-        }
+    private void RemoveEmailMappingCode(String willRemoveEmail) {
+        ScheduledFuture<?> already = scheduler.schedule(() -> removeProcess(willRemoveEmail)
+                                                              , 5
+                                                              , TimeUnit.MINUTES);
     }
+
+    private void removeProcess(String email) {
+        emailMapping.remove(email);
+        //TODO 동시 스레드, 1회차 프로세스 취소
+    }
+
 
 }
