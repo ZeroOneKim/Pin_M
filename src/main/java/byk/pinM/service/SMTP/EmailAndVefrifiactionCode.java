38c1ac50e7c5addacc2674d0284d2fa2547b84b6
Yungil Kim <wowwow0124@naver.com>
2023-09-01 20:26:06 +0900

    Email Ver_1.1

--- src/main/java/byk/pinM/service/SMTP/EmailAndVefrifiactionCode.java
+++ src/main/java/byk/pinM/service/SMTP/EmailAndVefrifiactionCode.java
@@ -3,19 +3,34 @@ package byk.pinM.service.SMTP;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Component
 public class EmailAndVefrifiactionCode {
     private HashMap<String, String> emailMapping = new HashMap<>();
+    private Timer timer = new Timer();
 
     public void setEmailMapping(String email, String code) {
         this.emailMapping.put(email, code);
+        timer.schedule(new RemoveEmailMappingCode(email), 15000);
     }
 
-    public boolean isvalidCheckCode(String email, String password) {
+    public boolean isValidCheckCode(String email, String password) {
         if(emailMapping.get(email) != null && emailMapping.get(email).equals(password)) return true;
         return false;
     }
 
+    private class RemoveEmailMappingCode extends TimerTask {
+        private String willRemoveEmail;
+
+        private RemoveEmailMappingCode(String willRemoveEmail) {
+            this.willRemoveEmail = willRemoveEmail;
+        }
+
+        public void run() {
+            emailMapping.remove(willRemoveEmail);
+        }
+    }
 
 }
