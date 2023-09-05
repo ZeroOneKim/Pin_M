65bf5c008b49f273fb2a7c0bf7c4d3df4bebee85
Yungil Kim <wowwow0124@naver.com>
2023-09-05 22:22:16 +0900

    JDBC Auth

--- src/main/java/byk/pinM/config/SecurityConfiguration.java
+++ src/main/java/byk/pinM/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,23 +12,41 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests() //TODO csrf 방어 추가
-                .mvcMatchers("/", "/signUp","/signUpEmailChk","/signUp_Process", "/signIn", "/signUpEmailChkWithNum",
+                .mvcMatchers("/", "/signUp","/signUpEmailChk","/signUp_Process", "/signUpEmailChkWithNum",
                         "/imgs/*","/login-link", "/asd").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    //.loginPage("/signIn")
+                    .loginPage("/signIn").permitAll()
                     .and()
                 .logout()
                     .permitAll()
                     .logoutSuccessUrl("/");
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT USER_ID, PASSWORD, 1 FROM SY_USER_MT WHERE USER_ID = ?")
+                .authoritiesByUsernameQuery("SELECT USER_ID, 'ADMIN' FROM SY_USER_MT WHERE USER_ID = ?");
     }
 
     //static 폴더 하위는 모두 접근가능 ex) css/ js ....
