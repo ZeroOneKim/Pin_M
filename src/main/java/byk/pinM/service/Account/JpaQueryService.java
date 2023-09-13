4a500efcd6621b6aef0b4e65500141d46ef72271
Yungil Kim <wowwow0124@naver.com>
2023-09-13 22:03:22 +0900

    err fix

--- src/main/java/byk/pinM/service/Account/JpaQueryService.java
+++ src/main/java/byk/pinM/service/Account/JpaQueryService.java
@@ -3,16 +3,22 @@ package byk.pinM.service.Account;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 
 @Service
 public class JpaQueryService {
     private EntityManager em;
 
+    public JpaQueryService(EntityManager em) {
+        this.em = em;
+    }
+
     //TODO JPQL
     public String SimpleSelectTable(String query) {
-        TypedQuery<String> queryExe = em.createQuery(query, String.class);
+        String queryExe = em.createQuery(query, String.class).getSingleResult();
 
-        return "";
+
+        return queryExe;
     }
+
+
 }
