d5a281e6c6a28311cf839d6e99762060fd361be4
Yungil Kim <wowwow0124@naver.com>
2023-09-13 22:04:20 +0900

    JPQL

--- src/main/java/byk/pinM/controller/content/MainController.java
+++ src/main/java/byk/pinM/controller/content/MainController.java
@@ -1,13 +1,21 @@
 package byk.pinM.controller.content;
 
+import byk.pinM.service.Account.MainContentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    private MainContentService mainContentService;
+
+    public MainController(MainContentService mainContentService) {
+        this.mainContentService = mainContentService;
+    }
 
     @GetMapping("/content")
     public String mainPage() {
+        System.out.println(mainContentService.getUserNickname());
+
         return "content/main";
     }
 }
