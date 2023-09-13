d48b985d1dda2572aa0503db461ef06120cac32a
Yungil Kim <wowwow0124@naver.com>
2023-09-13 22:03:46 +0900

    get user Data

--- src/main/java/byk/pinM/service/Account/MainContentService.java
+++ src/main/java/byk/pinM/service/Account/MainContentService.java
@@ -6,9 +6,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MainContentService {
+    private JpaQueryService jpaQueryService;
+
+    public MainContentService(JpaQueryService jpaQueryService) {
+        this.jpaQueryService = jpaQueryService;
+    }
+
+
     public String getUserNickname() {
         String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
+        String query = "select nickname from User where user_id = '" + user_id + "'";
 
-        return user_id;
+
+        String res = jpaQueryService.SimpleSelectTable(query);
+        System.out.println(res);
+        return res;
     }
 }
