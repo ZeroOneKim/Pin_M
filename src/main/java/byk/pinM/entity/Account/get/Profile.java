1e9175fc10130c221341ddc31082e4de33bb8359
Yungil Kim <wowwow0124@naver.com>
2023-09-16 20:36:43 +0900

    profile Enti

---
+++ src/main/java/byk/pinM/entity/Account/get/Profile.java
@@ -0,0 +1,65 @@
+package byk.pinM.entity.Account.get;
+
+import byk.pinM.entity.Account.User;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Profile {
+    @Id
+    private String nickname;
+
+    private String email;
+    private String phone;
+    private String address;
+    private String address2;
+
+    public Profile(User user) {
+        this.nickname = user.getNickname();
+        this.email    = user.getEmail();
+        this.phone    = user.getPhone();
+        this.address  = user.getAddress();
+        this.address2 = user.getAddress2();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+}
