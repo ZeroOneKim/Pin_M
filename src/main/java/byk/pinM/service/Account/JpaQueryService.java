b66f8d25ec9348503fa6d2db425cf96e8efa4bea
Yungil Kim <wowwow0124@naver.com>
2023-09-16 09:43:18 +0900

    add java doc

--- src/main/java/byk/pinM/service/Account/JpaQueryService.java
+++ src/main/java/byk/pinM/service/Account/JpaQueryService.java
@@ -5,17 +5,26 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 
+/**
+ * JPA/ JPQL 관련 쿼리 실행 메서드
+ * @author yikim
+ * @version 1.0
+ */
 @Service
 public class JpaQueryService {
     @Autowired private EntityManager entityManager;
 
-    //TODO JPQL
+    /**
+     * 쿼리 실행 결과를 String 형태로 단일 값 목적으로 return 한다.
+     * @param query : 실행될 쿼리.
+     */
     public String SimpleSelectTable(String query) {
         String queryExe = entityManager.createQuery(query, String.class).getSingleResult();
 
-
         return queryExe;
     }
+
+
 
 
 }
--- src/main/java/byk/pinM/service/Account/MainContentService.java
+++ src/main/java/byk/pinM/service/Account/MainContentService.java
@@ -4,19 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+/**
+ * MainContent Page에서 사용할 DB정보 및 데이터를 가져옴.
+ * @author yikim
+ * @version 1.0
+ */
 
 @Service
 public class MainContentService {
     @Autowired
     private JpaQueryService jpaQueryService;
 
-
     public String getUserNickname() {
         String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
         String query = "select nickname from User where user_id = '" + user_id + "'";
 
-
         String res = jpaQueryService.SimpleSelectTable(query);
         return res;
     }
+
+
 }
