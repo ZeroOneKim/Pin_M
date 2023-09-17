d4cf83e8c712147cdebcfa4574921b0ac5c87b0b
Yungil Kim <wowwow0124@naver.com>
2023-09-17 21:15:33 +0900

    add userinfo JPA

--- src/main/java/byk/pinM/repository/AccountRepository.java
+++ src/main/java/byk/pinM/repository/AccountRepository.java
@@ -4,11 +4,15 @@ import byk.pinM.entity.Account.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 
 @Transactional(readOnly = true)
 public interface AccountRepository extends JpaRepository<User, String> {
     boolean existsById(String user_id);
 
     boolean existsByNickname(String nickname);
+
+    Optional<User> findById(String userId);
 
 }
