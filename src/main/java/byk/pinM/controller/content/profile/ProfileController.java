f63a3c10b4a8063219d27a5ad611ebda6d111cc3
Yungil Kim <wowwow0124@naver.com>
2023-09-20 19:45:22 +0900

    content - Password 변경 페이지 추가

--- src/main/java/byk/pinM/controller/content/profile/ProfileController.java
+++ src/main/java/byk/pinM/controller/content/profile/ProfileController.java
@@ -3,22 +3,30 @@ package byk.pinM.controller.content.profile;
 import byk.pinM.entity.Account.User;
 import byk.pinM.entity.Account.get.Profile;
 import byk.pinM.repository.AccountRepository;
+import byk.pinM.service.Account.AccountJpaService;
 import byk.pinM.service.Account.MainContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
+import javax.validation.Valid;
 import java.util.Optional;
 
+/**
+ * @explain : URL -> content 하위에 있는 서비스 Page
+ * @author : yikim
+ * 최종수정 : 2023-09-20
+ */
 @Controller
 public class ProfileController {
     @Autowired private MainContentService mainContentService;
     @Autowired private AccountRepository accountRepository;
+    @Autowired private AccountJpaService accountJpaService;
 
     @GetMapping("/content/profile")
     public String profileForm(Model model) {
@@ -38,8 +46,21 @@ public class ProfileController {
     }
 
     @PostMapping("/profile/update")
-    public String profileUpdate(@ModelAttribute Profile profile) {
-        //Validated 사용법 파악
-        return null;
+    public String profileUpdate(@Valid @ModelAttribute Profile profile, Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute(profile);
+            System.out.println("오류 발견");
+            System.out.println(errors);
+            return "content/profile";
+        }
+
+        accountJpaService.updateProfile(profile);
+
+        return "redirect:/content";
+    }
+
+    @GetMapping("/content/password")
+    public String passwordPage() {
+        return "content/password";
     }
 }
