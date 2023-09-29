3f1900ba1e42202a5c63f6edcbd1641aa39e6a11
Yungil Kim <wowwow0124@naver.com>
2023-09-29 12:01:42 +0900

    add getter setter

--- src/main/java/byk/pinM/entity/pinservice/PinAccount.java
+++ src/main/java/byk/pinM/entity/pinservice/PinAccount.java
@@ -8,5 +8,23 @@ import javax.persistence.Id;
 public class PinAccount {
     @Id
     @Column(unique = true)
+    private String user_id;
+
     private String accountnum;
+    private String bank_nm;
+    private String bank_userNm;
+
+    public String getUser_id() { return user_id; }
+    public void setUser_id(String user_id) { this.user_id = user_id; }
+
+    public String getAccountnum() { return accountnum; }
+    public void setAccountnum(String accountnum) { this.accountnum = accountnum; }
+
+    public String getBank_nm() { return bank_nm; }
+    public void setBank_nm(String bank_nm) { this.bank_nm = bank_nm; }
+
+    public String getBank_userNm() { return bank_userNm; }
+    public void setBank_userNm(String bank_userNm) { this.bank_userNm = bank_userNm; }
+
+
 }
