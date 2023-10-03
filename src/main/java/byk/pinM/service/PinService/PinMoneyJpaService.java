3b0ebbc12b5e102a9190d31eda8715a82e66509d
Yungil Kim <wowwow0124@naver.com>
2023-10-03 17:15:54 +0900

    ADD JPA

--- src/main/java/byk/pinM/service/PinService/PinMoneyJpaService.java
+++ src/main/java/byk/pinM/service/PinService/PinMoneyJpaService.java
@@ -1,9 +1,26 @@
 package byk.pinM.service.PinService;
 
+import byk.pinM.entity.Account.User;
+import byk.pinM.entity.pinservice.PinAccount;
+import byk.pinM.repository.AccountRepository;
+import byk.pinM.repository.PinAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PinMoneyJpaService {
+    @Autowired private PinAccountRepository pinAccountRepository;
+    @Autowired private AccountRepository accountRepository;
 
+    public void PinAccountRegist(PinAccount pinAccount) {
+        pinAccount.setUser_id(SecurityContextHolder.getContext().getAuthentication().getName());
+        pinAccountRepository.save(pinAccount);
 
+        Optional<User> oUser = accountRepository.findById(SecurityContextHolder.getContext().getAuthentication().getName());
+        oUser.get().setRole_id(2);
+        accountRepository.save(oUser.get());
+    }
 }
