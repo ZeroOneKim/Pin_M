7a4911ad2d83375f4d9e337bd5187c03c4a6b906
Yungil Kim <wowwow0124@naver.com>
2023-10-03 17:15:38 +0900

    ORG

--- src/main/java/byk/pinM/controller/content/pinService/RegistAccount.java
+++ src/main/java/byk/pinM/controller/content/pinService/RegistAccount.java
@@ -1,12 +1,34 @@
 package byk.pinM.controller.content.pinService;
 
+import byk.pinM.entity.pinservice.PinAccount;
+import byk.pinM.repository.PinAccountRepository;
+import byk.pinM.service.PinService.PinMoneyJpaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistAccount {
+    @Autowired private PinAccountRepository pinAccountRepository;
+    @Autowired private PinMoneyJpaService pinMoneyJpaService;
+
     @GetMapping("/content/registAccount")
-    public String RegistAccountView() {
+    public String RegistAccountView(Model model) {
+
+        if(!pinAccountRepository.existsById(SecurityContextHolder.getContext().getAuthentication().getName())) {
+            model.addAttribute("pinaccount", new PinAccount());
+        }
+
         return "/content/registAccount";
+    }
+
+    @PostMapping("/content/registAccount-process")
+    public String RegistAccountProcess(PinAccount pinAccount) {
+        pinMoneyJpaService.PinAccountRegist(pinAccount);
+
+        return "redirect:/content";
     }
 }
