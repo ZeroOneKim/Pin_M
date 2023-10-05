549a1c6d12276fc56c510d00a44625f85aef8c5a
Yungil Kim <wowwow0124@naver.com>
2023-10-05 21:27:16 +0900

    클래스명 변경 및 에러 픽스

--- src/main/java/byk/pinM/controller/content/pinService/RegistAccountNumController.java
+++ src/main/java/byk/pinM/controller/content/pinService/RegistAccountNumController.java
@@ -2,6 +2,7 @@ package byk.pinM.controller.content.pinService;
 
 import byk.pinM.entity.pinservice.PinAccount;
 import byk.pinM.repository.PinAccountRepository;
+import byk.pinM.service.Account.MainContentService;
 import byk.pinM.service.PinService.PinMoneyJpaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,17 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class RegistAccount {
+public class RegistAccountNumController {
     @Autowired private PinAccountRepository pinAccountRepository;
     @Autowired private PinMoneyJpaService pinMoneyJpaService;
+    @Autowired private MainContentService mainContentService;
 
     @GetMapping("/content/registAccount")
     public String RegistAccountView(Model model) {
-
+        model.addAttribute("nickname", mainContentService.getUserNickname());
         if(!pinAccountRepository.existsById(SecurityContextHolder.getContext().getAuthentication().getName())) {
             model.addAttribute("pinaccount", new PinAccount());
         }
-
+        model.addAttribute("pinaccount", new PinAccount());
         return "/content/registAccount";
     }
 
