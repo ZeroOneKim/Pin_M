68889108437073c201b2c90dabb7f5854dfdb277
Yungil Kim <wowwow0124@naver.com>
2023-10-08 19:13:39 +0900

    err fix

--- src/main/java/byk/pinM/service/PinService/CrawlingWeatherService.java
+++ src/main/java/byk/pinM/service/PinService/CrawlingWeatherService.java
@@ -15,9 +15,10 @@ import java.util.*;
 @Service
 public class CrawlingWeatherService {
     private static final Logger logger = LogManager.getLogger("클래스 : " + CrawlingWeatherService.class);
-    public String tomo_AMPM_Temperature() {
+
+    public List<String> tomo_AMPM_Temperature() {
         String URL = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%A0%84%EA%B5%AD%EB%82%A0%EC%94%A8";
-        String answer = "";
+        List<String> answer = new ArrayList<>();
         try {
             Document doc = Jsoup.connect(URL).get();
             Elements temperatureElements = doc.select("a.lcl_a.local_01._marker .lcl_value._temperature");
@@ -25,10 +26,10 @@ public class CrawlingWeatherService {
             Element tomorrowTempA = temperatureElements.get(3);
             Element tomorrowTempB = temperatureElements.get(4);
 
-            answer = "내일 오전 :" + tomorrowTempA.text()
-                    + "   내일 오후 :" + tomorrowTempB.text();
+            answer.add("내일 오전 :"+tomorrowTempA.text());
+            answer.add("내일 오후 :"+tomorrowTempB.text());
         } catch(Exception e) {
-
+            logger.error(e);
         }
         return answer;
     }
@@ -40,19 +41,32 @@ public class CrawlingWeatherService {
             Document doc = Jsoup.connect(URL).get();
             Elements tempElement = doc.select(".scroll_box._horizontal_scroll .graph_inner._hourly_weather .degree_point .num");
             Elements rainElement = doc.select(".scroll_box._horizontal_scroll._hourly_rain .climate_box .icon_wrap .value");
+            Elements timeElement = doc.select(".scroll_box._horizontal_scroll .graph_inner._hourly_weather .time");
 
+            boolean timeCnt = false;
+            int forCnt = 26;
             Map<String, String> map2 = new LinkedHashMap<>();
-            for(int i = 9; i < 33; i++) {
-                String hour = i-9+"시";
-                Element data1 = tempElement.get(i);
-                Element data2 = rainElement.get(i);
-                Map<String, String> map = new LinkedHashMap<>();
+            for(int i = 0; i < forCnt; i++) {
+                if(timeCnt) {
+                    Element timeData = timeElement.get(i);
+                    Element data1 = tempElement.get(i);
+                    Element data2 = rainElement.get(i);
+                    Map<String, String> map = new LinkedHashMap<>();
 
-                map.put("시간", hour);
-                map.put("기온", data1.text());
-                map.put("강수확률", data2.text());
+                    map.put("시간", timeData.text());
+                    map.put("기온", data1.text());
+                    map.put("강수확률", data2.text());
 
-                answer.add(map);
+                    answer.add(map);
+                }
+
+                if(!timeCnt) {
+                    Element test = timeElement.get(i);
+                    if(test.text().equals("내일")) {
+                        timeCnt=true;
+                        forCnt = i+24;
+                    }
+                }
             }
         } catch (Exception e) {
             answer.add("ERR] 날짜 정보를 얻지 못했습니다.");
