0edb33bbff46dc380c892e2df7475ee1aa8d1cd2
Yungil Kim <wowwow0124@naver.com>
2023-10-14 22:05:03 +0900

    Mapping with Tables

--- src/main/java/byk/pinM/entity/pinservice/PinPointRecord.java
+++ src/main/java/byk/pinM/entity/pinservice/PinPointRecord.java
@@ -3,10 +3,12 @@ package byk.pinM.entity.pinservice;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
+@Table(name = "PIN_POINT_RECORD_MT")
 public class PinPointRecord implements Serializable {
     @Id
     private Date record_dt;
