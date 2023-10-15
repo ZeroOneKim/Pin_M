6d0a65a9006725153722e720e877220e11fb4210
Yungil Kim <wowwow0124@naver.com>
2023-10-15 18:04:49 +0900

    포인트 추가 WITH JPA

--- src/main/java/byk/pinM/controller/content/pinService/WeatherController.java
+++ src/main/java/byk/pinM/controller/content/pinService/WeatherController.java
@@ -1,7 +1,11 @@
 package byk.pinM.controller.content.pinService;
 
+import byk.pinM.entity.pinservice.PinPoint;
 import byk.pinM.entity.pinservice.PinPointRecord;
+import byk.pinM.repository.PinPointRecordRepository;
+import byk.pinM.repository.PinPointRepository;
 import byk.pinM.service.Account.MainContentService;
+import byk.pinM.service.JPA.JpaQueryService;
 import byk.pinM.service.PinService.CrawlingWeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,13 +14,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class WeatherController {
     @Autowired private CrawlingWeatherService crawlingWeatherService;
     @Autowired private MainContentService mainContentService;
+    @Autowired private JpaQueryService jpaQueryService;
+    @Autowired private PinPointRecordRepository pinPointRecordRepository;
+    @Autowired private PinPointRepository pinPointRepository;
 
     @GetMapping("/content/weather")
     public String weatherView(Model model) {
@@ -30,18 +39,25 @@ public class WeatherController {
         return "content/weather";
     }
 
+    //TODO 도중에 여러번 실행되면?
     @PostMapping("/check-weather") //미완
     public String weatherCheck() {
         try {
-            PinPointRecord checkingWeather = new PinPointRecord()
-                    .builder().user_id(SecurityContextHolder.getContext().getAuthentication().getName())
-                    .mission_id(1).price() //TODO Query Price data
+            int price = mainContentService.getPriceByMission(1);
+
+            PinPointRecord checkingWeather = PinPointRecord.builder()
+                    .user_id(SecurityContextHolder.getContext().getAuthentication().getName())
+                    .mission_id(1).price(price)
                     .build();
+            pinPointRecordRepository.save(checkingWeather);
 
-            //TODO Pin POINT MT 추가처리. DB작업
-
+            Optional<PinPoint> objPoint = pinPointRepository.findById(SecurityContextHolder.getContext().getAuthentication().getName());
+            //TODO 정리 + HTML 파일 수정.
+            int newPoint = objPoint.get().getPin_point() + price;
+            objPoint.get().setPin_point(newPoint);
+            pinPointRepository.save(objPoint.get());
         } catch (Exception e) {
-            //TODO
+            e.printStackTrace();
         }
         return "redirect:/content/weather";
     }
