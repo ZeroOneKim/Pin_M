48d79d333339a35a7e4a15db9959d19a5d7c0dc0
Yungil Kim <wowwow0124@naver.com>
2023-10-19 21:18:21 +0900

    생성 메서드 호출

--- src/main/java/byk/pinM/controller/content/pinService/WeatherController.java
+++ src/main/java/byk/pinM/controller/content/pinService/WeatherController.java
@@ -1,14 +1,11 @@
 package byk.pinM.controller.content.pinService;
 
-import byk.pinM.entity.pinservice.PinPoint;
-import byk.pinM.entity.pinservice.PinPointRecord;
 import byk.pinM.repository.PinPointRecordRepository;
 import byk.pinM.repository.PinPointRepository;
 import byk.pinM.service.Account.MainContentService;
 import byk.pinM.service.JPA.JpaQueryService;
 import byk.pinM.service.PinService.CrawlingWeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 public class WeatherController {
@@ -42,23 +38,9 @@ public class WeatherController {
     //TODO 도중에 여러번 실행되면?
     @PostMapping("/check-weather") //미완
     public String weatherCheck() {
-        try {
-            int price = mainContentService.getPriceByMission(1);
 
-            PinPointRecord checkingWeather = PinPointRecord.builder()
-                    .user_id(SecurityContextHolder.getContext().getAuthentication().getName())
-                    .mission_id(1).price(price)
-                    .build();
-            pinPointRecordRepository.save(checkingWeather);
+        mainContentService.WeatherChkSuccess(1);
 
-            Optional<PinPoint> objPoint = pinPointRepository.findById(SecurityContextHolder.getContext().getAuthentication().getName());
-            //TODO 정리 + HTML 파일 수정.
-            int newPoint = objPoint.get().getPin_point() + price;
-            objPoint.get().setPin_point(newPoint);
-            pinPointRepository.save(objPoint.get());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return "redirect:/content/weather";
     }
 }
