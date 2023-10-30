89a7d44c26a175e67fefd7d83c2f93e59517d587
Yungil Kim <wowwow0124@naver.com>
2023-10-30 21:55:10 +0900

    가독성 향상

--- src/main/java/byk/pinM/service/JPA/PinMoneyJpaService.java
+++ src/main/java/byk/pinM/service/JPA/PinMoneyJpaService.java
@@ -1,11 +1,13 @@
 package byk.pinM.service.JPA;
 
 import byk.pinM.entity.Account.User;
+import byk.pinM.entity.Account.UserLog;
 import byk.pinM.entity.pinservice.PinAccount;
 import byk.pinM.entity.pinservice.PinPoint;
 import byk.pinM.repository.AccountRepository;
 import byk.pinM.repository.PinAccountRepository;
 import byk.pinM.repository.PinPointRepository;
+import byk.pinM.repository.UserLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -22,29 +24,31 @@ import java.util.Optional;
 public class PinMoneyJpaService {
     @Autowired private PinAccountRepository pinAccountRepository; //계좌 레퍼지토리
     @Autowired private AccountRepository accountRepository; //계정 레퍼지토리
+    @Autowired private UserLog userLog;
+    @Autowired private UserLogRepository userLogRepository;
     @Autowired private PinPointRepository pinPointRepository;
 
     /**
      * 계좌 등록정보를 받은 후 계좌 생성, 계정 정보의 권한 변경, PinPoint 관련 DB정보 생성
      * @param pinAccount : 계정정보
      */
-    public void pinAccountRegist(PinAccount pinAccount) {
-        pinAccount.setUser_id(SecurityContextHolder.getContext().getAuthentication().getName());
+    public void pinAccountRegist(PinAccount pinAccount, String user_id) {
+        pinAccount.setUser_id(user_id);
         pinAccountRepository.save(pinAccount);
 
-        Optional<User> oUser = accountRepository.findById(SecurityContextHolder.getContext().getAuthentication().getName());
+        Optional<User> oUser = accountRepository.findById(user_id);
         oUser.get().setRole_id(2);
         accountRepository.save(oUser.get());
 
         PinPoint pinPoint = PinPoint.builder()
-                .user_id(SecurityContextHolder.getContext().getAuthentication().getName())
+                .user_id(user_id)
                 .pin_point(0)
                 .use_point(0).build();
         pinPointRepository.save(pinPoint);
     }
 
 
-    public void weatherCheck() {
+    public void addIpInformation() {
         String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
 
 
