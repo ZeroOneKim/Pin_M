f491846b5034dea00585b74ba4604786e81496e1
Yungil Kim <wowwow0124@naver.com>
2023-10-31 21:52:27 +0900

    불필요코드 제거

--- src/main/java/byk/pinM/entity/Account/UserLog.java
+++ src/main/java/byk/pinM/entity/Account/UserLog.java
@@ -1,16 +1,13 @@
 package byk.pinM.entity.Account;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "sy_user_log_ht")
 public class UserLog {
     @Id
-    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long log_id;
 
     private String user_id;
@@ -33,36 +30,4 @@ public class UserLog {
     public String getPage_nm() { return page_nm; }
     public void setPage_nm(String page_nm) { this.page_nm = page_nm; }
 
-
-    public UserLogBuilder builder() {
-        return new UserLogBuilder();
-    }
-
-    public class UserLogBuilder {
-        UserLog userLog = new UserLog();
-        public UserLogBuilder logId(Long log_id) {
-            userLog.setLog_id(log_id);
-            return this;
-        }
-        public UserLogBuilder userId (String user_id) {
-            userLog.setUser_id(user_id);
-            return this;
-        }
-        public UserLogBuilder AccessDate(String access_dt) {
-            userLog.setAccess_dt(access_dt);
-            return this;
-        }
-        public UserLogBuilder ipAddress(String ip_addr) {
-            userLog.setIp_addr(ip_addr);
-            return this;
-        }
-        public UserLogBuilder pageNm(String page_nm) {
-            userLog.setPage_nm(page_nm);
-            return this;
-        }
-
-        public UserLog build() {
-            return userLog;
-        }
-    }
 }
